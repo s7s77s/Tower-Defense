92b67ef9e722d57942ae64e52adfac169087624f
krononpark <57452349+krononpark@users.noreply.github.com>
2024-03-30 11:32:07 +0300

    sssss

--- load.py
+++ load.py
@@ -1,6 +1,6 @@
 import pygame
 
-enemy = pygame.image.load('image/enemy/right.png')
+enemy_image = pygame.image.load('image/enemy/right.png')
 bottom_image = pygame.image.load('image/tile map/bottom.png')
 bush_image = pygame.image.load('image/tile map/bush_tile 1.png')
 bush_tower_image = pygame.image.load('image/tile map/bush_tile_tower.png')
@@ -8,3 +8,8 @@ grass_image = pygame.image.load('image/tile map/grass_tile_1.png')
 left_image = pygame.image.load('image/tile map/left.png')
 rigth_image = pygame.image.load('image/tile map/right.png')
 top_image = pygame.image.load('image/tile map/top.png')
+toweron_image = pygame.image.load('image/tower 1/1_on.png')
+towerof_image = pygame.image.load('image/tower 1/1_of.png')
+bullet_image = pygame.image.load('image/tower 1/bullet 1.png')
+panel_image = pygame.image.load('image/panel.jpg')
+upgrade_image = pygame.image.load('image/upgrade.png')
--- main.py
+++ main.py
@@ -39,17 +39,46 @@ def drawMaps(nameFile):
                 bush_lite = Block(bush_tower_image, pos)
                 bush_tower_group.add(bush_lite)
             if maps[i][j] == '4':
-                bottom = Block(bottom_image, pos)
-                bottom_group.add(bottom)
-            if maps[i][j] == '5':
-                left = Block(left_image, pos)
-                left_group.add(left)
+                bottom = Edit_dir_tile(bottom_image, pos, 'bottom')
+                edit_dir_group.add(bottom)
             if maps[i][j] == '6':
-                right = Block(rigth_image, pos)
-                right_group.add(right)
+                right = Edit_dir_tile(rigth_image, pos, 'right')
+                edit_dir_group.add(right)
             if maps[i][j] == '7':
-                top = Block(top_image, pos)
-                top_group.add(top)
+                top = Edit_dir_tile(top_image, pos, 'top')
+                edit_dir_group.add(top)
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, image, pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+        self.dir = 'right'
+        self.speedx = 2
+        self.speedy = 0
+
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if pygame.sprite.spritecollide(self, edit_dir_group, False):
+            tile = pygame.sprite.spritecollide(self, edit_dir_group, False)[0]
+            if abs(self.rect.centerx - tile.rect.centerx) <= 5 and abs(self.rect.centery - tile.rect.centery) <= 5:
+                self.dir = tile.dir
+        if self.dir == 'right':
+            self.speedx = 2
+            self.speedy = 0
+            self.image = pygame.transform.rotate(enemy_image, 0)
+        if self.dir == 'top':
+            self.speedy = -2
+            self.speedx = 0
+            self.image = pygame.transform.rotate(enemy_image, 90)
+        if self.dir == 'bottom':
+            self.speedy = 2
+            self.speedx = 0
+            self.image = pygame.transform.rotate(enemy_image, 260)
 
 
 class Block(pygame.sprite.Sprite):
@@ -68,36 +97,58 @@ class Edit_dir_tile(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = pos[0]
         self.rect.y = pos[1]
+        self.dir = dir
+
+
+class Spawn():
+    def __init__(self):
+        self.timer_spawn = 0
+
+    def update(self):
+        self.timer_spawn += 1
+        if self.timer_spawn / FPS > 1:
+            enemy = Enemy(enemy_image, (-70, 570))
+            enemy_group.add(enemy)
+            self.timer_spawn = 0
+
+
+class TowerB(pygame.sprite.Sprite):
+    def __init__(self, image, pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect
+        self.rect.x = pos[0]
+        self.rect.y = pos[0]
+        self.buy = False
+        self.timer_click = 0
 
 
 def lvl_game():
     sc.fill('gray')
     bush_group.update()
     bush_group.draw(sc)
-    bottom_group.update()
-    bottom_group.draw(sc)
     bush_tower_group.update()
     bush_tower_group.draw(sc)
     grass_group.update()
     grass_group.draw(sc)
-    left_group.update()
-    left_group.draw(sc)
-    right_group.update()
-    right_group.draw(sc)
-    top_group.update()
-    top_group.draw(sc)
+    edit_dir_group.update()
+    edit_dir_group.draw(sc)
+    enemy_group.update()
+    enemy_group.draw(sc)
+    sc.blit(panel_image, (1, 700))
+    spawn.update()
     pygame.display.update()
 
 
 def restart():
-    global bottom_group, bush_group, bush_tower_group, grass_group, left_group, right_group, top_group
-    bottom_group = pygame.sprite.Group()
+    global edit_dir_group, bush_group, bush_tower_group, grass_group, enemy_group, spawn, towerb_group
     bush_group = pygame.sprite.Group()
     bush_tower_group = pygame.sprite.Group()
     grass_group = pygame.sprite.Group()
-    left_group = pygame.sprite.Group()
-    right_group = pygame.sprite.Group()
-    top_group = pygame.sprite.Group()
+    enemy_group = pygame.sprite.Group()
+    edit_dir_group = pygame.sprite.Group()
+    spawn = Spawn()
+    towerb_group = pygame.sprite.Group()
 
 
 restart()
